62fbb10a46e02de7cde78f1aadd9d6195b06bbfe
John Guest <jcguest90@gmail.com>
2019-08-14 22:40:36 +0000

    Automatically backed up by Learn

--- countdown.rb
+++ countdown.rb
@@ -9,4 +9,13 @@ def countdown(i)
   
 end
 
+def countdown_with_sleep(seconds_to_midnight)
+  while seconds_to_midnight > 0
+    puts "#{seconds_to_midnight} SECOND(S)!"
+    sleep(1)
+    seconds_to_midnight -= 1
+  end
+  "HAPPY NEW YEAR!"
+end
 
+countdown_with_sleep(10)
